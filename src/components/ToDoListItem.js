d0a6129103cf0815074882b24cd9422df9790a84
Simran Suky Kaur <simransukhi26@gmail.com>
2022-12-09 16:21:24 +0100

    checkbox functionality

--- src/components/ToDoListItem.js
+++ src/components/ToDoListItem.js
@@ -1,8 +1,15 @@
+import { useState } from "react";
 export default function TodoListItem({taskName}){
+    
+    const [isChecked, setIsChecked] = useState(false);
+    function checkBoxHandler(){
+        setIsChecked(!isChecked)
+
+    }
     return (
             <li>
-                <input className="completed" type="checkbox" />
-                <span className="taskName">{taskName.input}</span>
+                <input className="completed" type="checkbox" onChange={checkBoxHandler}/>
+                <span className= {isChecked? "checkedLook":"taskName"}>{taskName.input}</span>
                 <button className="delButton">Delete</button>
             </li>
     );
